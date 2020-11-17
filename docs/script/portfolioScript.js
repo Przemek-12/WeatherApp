2723eeae3caf23e4c55bff380caff0fa264d122d
Przemek-12 <pecework@gmail.com>
2020-11-17 18:13:06 +0100

    weather

--- docs/script/portfolioScript.js
+++ docs/script/portfolioScript.js
@@ -3,15 +3,15 @@ window.addEventListener('load', ()=>{
     var folder = "media/portfolio/";
 
     for(var i=1; i<15; i++){
-        console.log(folder + i+'.jpg')
-        $("main").append( "<img id='"+i+"' src='"+ folder + i+'.jpg'+"'>" );
+        console.log(folder + i+'.JPG')
+        $("main").append( "<img id='"+i+"' src='"+ folder + i+'.JPG'+"'>" );
     }
 
     $('img').click((item)=>{
         var lightboxDisplay = $('#lightbox').css('display');
         if(lightboxDisplay==='none'){
             $('#lightbox').css('display', 'flex');
-            $('#lightboxImg').css('background-image', "url('media/portfolio/"+item.target.id+".jpg')");
+            $('#lightboxImg').css('background-image', "url('media/portfolio/"+item.target.id+".JPG')");
 
         }
         else{
